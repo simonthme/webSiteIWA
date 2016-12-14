a2305d43ff80c93beb740d83ea85addaac5c9ea7
simonthme <thome.simon@gmail.com>
2016-12-14 15:16:16 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by simonthome on 14/12/2016.
+ */
+import React from 'react';
+import {render} from 'react-dom';
+
+import {browserHistory} from 'react-router';
+
+import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
+
+
+render(<div></div>, document.querySelector('#app'));
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+/**
+ * Created by simonthome on 23/11/2016.
+ */
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: [
+    'webpack-dev-server/client?http://127.0.0.1:8080/',
+    'webpack/hot/only-dev-server',
+    './src'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    modulesDirectories: ['node_modules', 'src'],
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ['babel?presets[]=react,presets[]=es2015,presets[]=stage-2']
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      },
+      {
+        test: /\.png$/,
+        loader: "url-loader?limit=100000"
+      },
+      {
+        test: /\.jpg$/,
+        loader: "file-loader"
+      },
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/octet-stream'
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file'
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=image/svg+xml'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]
+};
